946e44b916c2ed2b05a06a36c1cd7837a5195e7f
thinhpham311-dev <phamthinh30522@gmail.com>
2026-01-19 21:36:48 +0700

    update carousel stories ui responsive

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,50 @@
 $(document).ready(function () {
-  const $slider = $('.product-ugc__container');
+  const $slider = $('.product_ugc-container');
   if (!$slider.length || $slider.hasClass('slick-initialized')) return;
 
   $slider.slick({
     slidesToShow: 4,
+    slidesToScroll: 1,
     arrows: false,
     dots: true,
-    infinite: false,
     speed: 300,
     accessibility: false,
+    infinite: false,
+
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+         
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+   
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+              centerMode: true,
+        centerPadding: '40px',
+            infinite: true,
+
+          dots: true,
+        },
+      },
+    ],
   });
 
-  $('.product_carousel-prev').on('click', () => $slider.slick('slickPrev'));
-  $('.product_carousel-next').on('click', () => $slider.slick('slickNext'));
+  $('.product_carousel-prev').on('click', () => {
+    $slider.slick('slickPrev');
+  });
+
+  $('.product_carousel-next').on('click', () => {
+    $slider.slick('slickNext');
+  });
 });
